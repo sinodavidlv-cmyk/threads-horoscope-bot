3a73210a845a2b22613cbf6b3f84891e452b18ed
sinodavidlv-cmyk <sinodavidlv@gmail.com>
2026-09-18 09:35:19 +0800

    Add product list to products.py

---
+++ products.py
@@ -0,0 +1,22 @@
+products = [
+    {
+        "name": "水晶能量小物",
+        "url": "https://s.shopee.tw/2g8Wq6DKc7"
+    },
+    {
+        "name": "療癒水晶飾品",
+        "url": "https://s.shopee.tw/1Ld9Go2FOh"
+    },
+    {
+        "name": "水晶手機鍊",
+        "url": "https://s.shopee.tw/7VFqhz1pQ9"
+    },
+    {
+        "name": "十二星座手鍊",
+        "url": "https://s.shopee.tw/7Ad0JTWISK"
+    },
+    {
+        "name": "天然水晶手鍊",
+        "url": "https://s.shopee.tw/5q7cjOXEpd"
+    }
+]
